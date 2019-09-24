322920161b27a9a19a63637a970be1b00f330976
johnomahoy <53034160+johnomahoy@users.noreply.github.com>
2019-09-24 09:53:35 +0800

    Update ednovationSMS.php

--- ednovationSMS.php
+++ ednovationSMS.php
@@ -10,11 +10,14 @@ if( $app->cfgCon("al477"))
 	$contactId = $_REQUEST['contactId'];  
 	// $contactId = 468; 
 
+	//Fetch the contact data using the contact id
 	$returnFields = array('Phone1','_BookingCentre'); 
 	$conDat = $app->dsLoad("Contact", $contactId, $returnFields);
 	
+	//Create a message
 	$message="See you at the Cambridge @ ".$conDat['_BookingCentre']." Open House on Saturday 9th March (10am to 12pm)!";
 	
+	//Format the message into a url format type
 	$url= "https://mx.fortdigital.net/http/send-message?username=83672&password=3edc5tgb&to=%2B65".urlencode($conDat['Phone1'])."&from=Cambridge&message=".urlencode($message);
 	// $url= "https://mx.fortdigital.net/http/send-message?username=83672&password=3edc5tgb&to=%2B639093140986&from=Cambridge&message=".urlencode($message); 
 	// create a new cURL resource 
