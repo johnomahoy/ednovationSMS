dfc0c680c3ea6df0d76d180bba7fdc145c4e08f7
johnomahoy <53034160+johnomahoy@users.noreply.github.com>
2019-09-24 09:48:55 +0800

    Add files via upload

---
+++ ednovationSMS.php
@@ -0,0 +1,34 @@
+<?php
+require_once("isdk.php");
+$app = new iSDK;
+//Test Connection
+if( $app->cfgCon("al477"))
+{
+	echo "You are connected to infusion soft";
+	
+	// $contactId = $_REQUEST['id'];  
+	$contactId = $_REQUEST['contactId'];  
+	// $contactId = 468; 
+
+	$returnFields = array('Phone1','_BookingCentre'); 
+	$conDat = $app->dsLoad("Contact", $contactId, $returnFields);
+	
+	$message="See you at the Cambridge @ ".$conDat['_BookingCentre']." Open House on Saturday 9th March (10am to 12pm)!";
+	
+	$url= "https://mx.fortdigital.net/http/send-message?username=83672&password=3edc5tgb&to=%2B65".urlencode($conDat['Phone1'])."&from=Cambridge&message=".urlencode($message);
+	// $url= "https://mx.fortdigital.net/http/send-message?username=83672&password=3edc5tgb&to=%2B639093140986&from=Cambridge&message=".urlencode($message); 
+	// create a new cURL resource 
+	$ch = curl_init();
+	// set URL and other appropriate options
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_HEADER, 0); 
+	// grab URL and pass it to the browser 
+	curl_exec($ch);
+	// close cURL resource, and free up system resources
+	curl_close($ch);
+	
+}else{
+echo "Not Connected";
+}
+
+?>
